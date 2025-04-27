7f510c2b773103c93784e945147115a5e331e645
KhiemNguyen2104 <khiem.nguyenphucgia@hcmut.edu.vn>
2025-04-27 23:17:55 +0700

    Default values for API properties

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -4,28 +4,28 @@ import { IsNotEmpty, IsString } from "class-validator";
 export class LoginDto {
     @IsNotEmpty()
     @IsString()
-    @ApiProperty({type: String, description: "User ID for the login", required: true})
+    @ApiProperty({type: String, description: "User ID for the login", required: true, default: "2211573"})
     userId: string
 
     @IsNotEmpty()
     @IsString()
-    @ApiProperty({type: String, description: "Password for the login", required: true})
+    @ApiProperty({type: String, description: "Password for the login", required: true, default: "123456"})
     password: string
 }
 
 export class SignupDto {
     @IsNotEmpty()
     @IsString()
-    @ApiProperty({type: String, description: "User ID for the register", required: true})
+    @ApiProperty({type: String, description: "User ID for the register", required: true, default: "2211573"})
     userId: string
 
     @IsNotEmpty()
     @IsString()
-    @ApiProperty({type: String, description: "User name for the register", required: true})
+    @ApiProperty({type: String, description: "User name for the register", required: true, default: "Nguyen Phuc Gia Khiem"})
     userName: string
 
     @IsNotEmpty()
     @IsString()
-    @ApiProperty({type: String, description: "Password for the register", required: true})
+    @ApiProperty({type: String, description: "Password for the register", required: true, default: "123456"})
     password: string
 }
