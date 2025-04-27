3cf0b14c7b3550fccdced92b5eb068daf55e510d
KhiemNguyen2104 <khiem.nguyenphucgia@hcmut.edu.vn>
2025-04-27 21:23:57 +0700

    Fix importing errors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports: [PrismaService]
+  imports: [PrismaModule, JwtModule.register({})]
 })
 export class AuthModule {}
