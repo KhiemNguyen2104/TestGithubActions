62a424e5d65dc6ceecd2d12b02f4c9531d668b69
KhiemNguyen2104 <khiem.nguyenphucgia@hcmut.edu.vn>
2025-04-27 22:13:26 +0700

    Update auth services

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -4,28 +4,28 @@ import { IsNotEmpty, IsString } from "class-validator";
 export class LoginDto {
     @IsNotEmpty()
     @IsString()
-    @ApiProperty({type: String, description: "User ID for the login", required: true, name: "User ID"})
+    @ApiProperty({type: String, description: "User ID for the login", required: true})
     userId: string
 
     @IsNotEmpty()
     @IsString()
-    @ApiProperty({type: String, description: "Password for the login", required: true, name: "Password"})
+    @ApiProperty({type: String, description: "Password for the login", required: true})
     password: string
 }
 
 export class SignupDto {
     @IsNotEmpty()
     @IsString()
-    @ApiProperty({type: String, description: "User ID for the register", required: true, name: "User ID"})
+    @ApiProperty({type: String, description: "User ID for the register", required: true})
     userId: string
 
     @IsNotEmpty()
     @IsString()
-    @ApiProperty({type: String, description: "User name for the register", required: true, name: "User Name"})
+    @ApiProperty({type: String, description: "User name for the register", required: true})
     userName: string
 
     @IsNotEmpty()
     @IsString()
-    @ApiProperty({type: String, description: "Password for the register", required: true, name: "Password"})
+    @ApiProperty({type: String, description: "Password for the register", required: true})
     password: string
 }
